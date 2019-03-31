e701dcefe8b3baf4df847522a2adb51bf559c08d
SAHRAENI MAULIDA <49121673+SAHRAENIMAULIDA@users.noreply.github.com>
2019-03-31 21:47:36 +0800

    Add files via upload

---
+++ uts1Maulida.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+int data_mahasiswa;
+    cout <<"nama : Sahraeni Maulida" <<endl;
+    cout<<"nim : 1829140002" <<endl;
+    cout<<"kelas : tekomB" <<endl;
+    cout<<"tahun angkatan : 2018" <<endl;
+}
+
+
+
+
+
+
